c463056aef64d96f96ef202c34ca3c66630a4fb2
disappeared <shenruizong@gmail.com>
2013-04-09 20:58:40 +0800

    添加edit 修复浏览器初始化。外加index添加一些内容

--- Admin/Lib/Action/NewsAction.class.php
+++ Admin/Lib/Action/NewsAction.class.php
@@ -2,29 +2,72 @@
 
 class NewsAction extends Action
 {
-    function Index()
-    {
-        $newsdb = new Model("news");
-        $news = $newsdb->select();
-        $this->assign("list",$news);
-        $this->display();
-    }
-    function Add()
-    {
-        $type = M("newstype");
-        $list = $type->select();
-        $this->assign("list",$list);
-        $this->display();
-    }
-    function AddDb()
-    {
-        $news = new NewsModel();
-        $news->create();
-        if($news->add())
-        {
-            $this->success("添加成功");
-        }  else {
-            dump($news);
-        }
-    }
+	function Index()
+	{
+		$newsdb = new Model("news");
+		$news = $newsdb->select();
+		$this->assign("list",$news);
+		$this->display();
+	}
+	function Add()
+	{
+		$type = M("newstype");
+		$list = $type->select();
+		$this->assign("list",$list);
+		$this->display();
+	}
+	function AddDb()
+	{
+		$news = new NewsModel();
+		$news->create();
+		if($news->add())
+		{
+			$this->success("添加成功");
+		}  else {
+			$this->error($news->getError());
+		}
+	}
+	function Del()
+	{
+		$id = $_GET['id'];
+		if($id)
+		{
+			$news = new NewsModel();
+			$result = $news->where("id = '".$id."'")->delete();
+			if($result)
+			{
+				$this->success("删除成功");
+			}else {
+				$this->error($news->getError());
+			}
+		}
+	}
+	function edit()
+	{
+		$id = $_GET['id'];
+		if($id)
+		{
+			$news = new NewsModel();
+			$list = $news->where("id='".$id."'")->find();
+			$this->assign("list",$list);
+			dump($list);
+			$this->display();
+		}else
+		{
+			$this->error("操作错误","__APP__");
+		}
+	}
+	function editDb()
+	{
+		$news = new NewsModel();
+		$news->create();
+		$result = $news->save();
+		if($result)
+		{
+			$this->success("保存成功");
+		}else
+		{
+			$this->error("保存失败。".$news->getError());
+		}
+	}
 }
